d359719c4d9ca7a688b17f672bde2caba9c2fa68
owestunilinkcom <owest@unilink.com>
2023-04-16 03:04:57 +0100

    Instantiate 2

--- libs/common-ui/src/lib/common-ui.module.ts
+++ libs/common-ui/src/lib/common-ui.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BannerComponent } from './banner/banner.component';
+import { LoaderComponent } from './loader/loader/loader.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [BannerComponent],
+  declarations: [BannerComponent, LoaderComponent],
   exports: [BannerComponent],
 })
 export class CommonUiModule {}
